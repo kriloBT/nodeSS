70c959bf02faa3fbf32ecc7ceb70280ad6221a4f
liyu <am997vivian@gmail.com>
2014-08-12 17:19:35 +0800

    delete

--- test_rmongo/examples/ex4.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var rio = require("../lib/rio");
-
-rio.evaluate("# ห\n2+2");
-rio.evaluate("# แผ่นดินฮั่นเสื่อมโทรมแสนสังเวช  พระปกเกศกองบู๊กู้ขึ้นใหม่\n65+4");
-rio.evaluate("# แผ่องบู๊กู้ขึ้นใหม่\n2*3");
--- test_rmongo/models/db.js
+++
@@ -1,9 +0,0 @@
-//db.js
-var settings = require('../settings');
-var Db = require('mongodb').Db;
-var conn = require('mongodb').Connection;
-var server = require('mongodb').Server;
-
-module.exports = new Db(settings.db, new server(settings.host, settings.port, {
-	auto_reconnect : true
-}));
