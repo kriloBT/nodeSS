a9fbe339829c307dc69c12564601fceb11fb954e
liyu <am997vivian@gmail.com>
2014-08-14 14:00:53 +0800

    Update user_amountPlotColor.R

--- nodeRmongo/routes/user_amountPlotColor.R
+++ nodeRmongo/routes/user_amountPlotColor.R
@@ -5,7 +5,7 @@ pipe_a <- mongo.bson.from.JSON('
         {\"_id\":\"$userID\", \"total\": {\"$sum\":\"$amount\"}}
     }');
 cmd_lista <- list(pipe_a);
-total <- mongo.aggregation(mongo, 'test2.c1', cmd_lista);
+total <- mongo.aggregation(mongo, 'test2.c2', cmd_lista);
 #total
 #dtotal <- list(0);
 ltotal <- mongo.bson.value(total, 'result');
